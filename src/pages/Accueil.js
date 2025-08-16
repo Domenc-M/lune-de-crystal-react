9ad23c8d6dd9969f62f811561379163212f696a2
maxime Domenc <malryck33@gmail.com>
2025-08-16 12:19:21 +0200

    map fix

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,7 +1,6 @@
  import React from 'react'
  import Navbar from '../components/Navbar'
  import homePhoto from '../img/devanture.jpg'
- import GoogleMap from '../components/GoogleMap'
 
  function Accueil() {
    return (
