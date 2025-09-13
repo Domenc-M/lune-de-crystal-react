f4f8510433e50ad42fa1b0d6c88552624d323fcc
maxime Domenc <malryck33@gmail.com>
2025-09-13 16:01:12 +0200

    changed h2 in accueil

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -29,14 +29,14 @@ Nous vous proposons une large gamme de <Link to="/produits" >produits</Link> don
         </section>
 
         <section>
-          <h2>Retrouvez nous à <strong>Orthez</strong>, au 3 rue du Général Foy.</h2>
+          <span className="subtitle">Retrouvez nous à <strong>Orthez</strong>, au 3 rue du Général Foy.</span>
           <div className="map-container">
             <iframe title="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46313.21810729475!2d-0.8159231771689421!3d43.49031544360271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd56937c6b53f033%3A0x1849b156a7d40881!2sLune%20de%20Crystal!5e0!3m2!1sen!2sfr!4v1755339262266!5m2!1sen!2sfr"></iframe>
           </div>
         </section>
 
         <section className="contact-section">
-        <h3>Vous pouvez aussi nous contacter...</h3>
+        <span className="subtitle">Vous pouvez aussi nous contacter...</span>
         <ul>
           <li>
             <p className="contact-title">Téléphone</p>
@@ -47,7 +47,7 @@ Nous vous proposons une large gamme de <Link to="/produits" >produits</Link> don
             <p className="contact-sub">lunedecrystal64@gmail.com</p>
           </li>
         </ul>
-        <h4>...Ou nous retrouver sur les réseaux sociaux !</h4>
+        <span className="subtitle">...Ou nous retrouver sur les réseaux sociaux !</span>
         <ul>
           <li>
             <a href="https://www.facebook.com/Lunedecrystal64" target="_blank" rel="noreferrer"><img src={facebook} alt="facebook"></img></a>
