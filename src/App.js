f646d0437131332184fceea9b148c838f9982e4c
maxime Domenc <malryck33@gmail.com>
2025-09-24 15:57:07 +0200

    fixed font pathing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Accueil from './pages/Accueil';
-import Produits from './pages/Produits';
-import Layout from './components/Layout';
-import Evenements from './pages/Evenements';
-import Prestations from './pages/Prestations';
-import ScrollToTop from './components/ScrollToTop';
-
-
 export default function App() {
   return (
     <div></div>
